301c86f5d50e6b76b5d21b6bb349f4169e14d215
Mario <m.inguito.484470@umindanao.edu.ph>
2024-02-04 22:20:38 +0800

    Introduction to Middleware

--- Core/router.php
+++ Core/router.php
@@ -1,5 +1,10 @@
 <?php
+
 namespace Core;
+
+use Core\Middleware\Guest;
+use Core\Middleware\Auth;
+use Core\Middleware\Middleware;
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -8,46 +13,61 @@ class Router
 {
     protected $routes = [];
 
-    public function add ($method, $uri, $controller){
+    public function add($method, $uri, $controller)
+    {
         $this->routes[] = [
             "uri" => $uri,
             "controller" => $controller,
-            "method" => $method
+            "method" => $method,
+            "middleware" => null
         ];
+        return $this;
     }
 
     public function get($uri, $controller)
     {
-        $this->add("GET", $uri, $controller);
+        return $this->add("GET", $uri, $controller);
     }
 
     public function post($uri, $controller)
     {
-        $this->add("POST", $uri, $controller);
+        return $this->add("POST", $uri, $controller);
     }
 
     public function delete($uri, $controller)
     {
-        $this->add("DELETE", $uri, $controller);
+        return $this->add("DELETE", $uri, $controller);
     }
 
     public function patch($uri, $controller)
     {
-        $this->add("PATCH", $uri, $controller);
+        return $this->add("PATCH", $uri, $controller);
     }
 
     public function put($uri, $controller)
     {
-        $this->add("PUT", $uri, $controller);
+        return $this->add("PUT", $uri, $controller);
+    }
+
+    public function only($key)
+    {
+        $this->routes[array_key_last($this->routes)]["middleware"] = $key;
+        return $this;
     }
 
     public function route($uri, $method)
     {
         foreach ($this->routes as $route) {
             if ($route["uri"] === $uri && $route["method"] === strtoupper($method)) {
+
+                if ($route["middleware"]) {
+                    $middleware = Middleware::MAP[$route["middleware"]];
+
+                    (new $middleware)->handle();
+                }
+
                 return require base_path($route["controller"]);
             }
-
         }
         $this->abort();
     }
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,7 +8,7 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <a href="/" class="<?= isCurrent("/") ?>  rounded-md px-3 py-2 text-sm font-medium">Home</a>
-                        <a href="/notes" class="<?= isCurrent("/notes" ) ?>  rounded-md px-3 py-2 text-sm font-medium">Notes</a>
+                        <a href="/notes" class="<?= isCurrent("/notes") ?>  rounded-md px-3 py-2 text-sm font-medium">Notes</a>
                         <a href="/about" class="<?= isCurrent("/about") ?> rounded-md px-3 py-2 text-sm font-medium">About</a>
                         <a href="/contact" class="<?= isCurrent("/contact") ?> rounded-md px-3 py-2 text-sm font-medium">Contacts</a>
                     </div>
@@ -26,18 +26,23 @@
 
                     <div class="relative ml-3">
                         <div>
-                            <button type="button" @click="isOpen = !isOpen" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span class="absolute -inset-1.5"></span>
-                                <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                            </button>
+
+                            <?php if ($_SESSION["user"] ?? false) : ?>
+                                <button type="button" @click="isOpen = !isOpen" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                    <span class="absolute -inset-1.5"></span>
+                                    <span class="sr-only">Open user menu</span>
+                                    <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                </button>
+                            <?php else : ?>
+                                <a href="/register" class="text-white">Register</a>
+                            <?php endif; ?>
+
                         </div>
-                        <div x-show="isOpen" x-transition:enter="transition ease-out duration-100 transform" x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-75 transform" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95"     class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg">
+                        <div x-show="isOpen" x-transition:enter="transition ease-out duration-100 transform" x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-75 transform" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg">
                             <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                                <!-- Active: "bg-gray-100", Not Active: "" -->
                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+                                <a href="/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
                             </div>
                         </div>
                     </div>
