fccfb1485f4c91ba62f590b9c19b118413a6d129
Mario <m.inguito.484470@umindanao.edu.ph>
2024-02-06 18:21:01 +0800

    Extract an Authenticar Class

---
+++ Http/Forms/LoginForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Http\Forms;
+
+use Core\Validator;
+
+class LoginForm
+{
+    protected $errors = [];
+
+    public function validate($email, $password)
+    {
+        $errors = [];
+
+        if (!Validator::email($email)) {
+            $this->errors['email'] = 'Please enter your email address.';
+        }
+
+        if (!Validator::string($password, 7, 255)) {
+            $this->errors['password'] = 'Please enter your password.';
+        }
+        return empty($errors);
+    }
+
+    public function errors()
+    {
+
+        return $this->errors;
+    }
+
+    public function error($field, $message)
+    {
+        $this->errors[$field] = $message;
+    }
+
+}
---
+++ Http/controllers/session/store.php
@@ -0,0 +1,30 @@
+<?php
+
+use Core\Authenticator;
+use Http\Forms\LoginForm;
+
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$form = new LoginForm();
+
+if (!$form->validate($email, $password)) {
+    return view('session/create.view.php', [
+        'errors' => $form->errors()
+    ]);
+}
+
+$auth = new Authenticator();
+
+if ($auth->attempt($email, $password)) {
+    redirect('/');
+}
+
+
+$form->error("email", "No matching account for that email address and password.");
+
+return view('session/create.view.php', [
+    'errors' => $form->errors()
+]);
